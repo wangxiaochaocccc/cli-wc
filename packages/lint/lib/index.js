8a5a9ea694ada64b638152de141472fd93784e37
wangchao <wangxiaochaocccc@163.com>
2023-03-30 11:02:26 +0800

    feat:ESLint API接入vue项目

--- packages/lint/lib/index.js
+++ packages/lint/lib/index.js
@@ -1,5 +1,7 @@
 import Command from '@learnmyself.com/command'
+import {log} from '@learnmyself.com/utils'
 import { ESLint} from 'eslint'
+import vueConfig from './eslint/vueConfig.js'
 class lintCommand extends Command {
   get command () {
     return 'lint'
@@ -14,11 +16,41 @@ class lintCommand extends Command {
   async action () {
     // eslint
     const cwd = process.cwd()
-    const eslint = new ESLint({ cwd })
-    const rules = await eslint.lintFiles(['**/*.js'])
+    const eslint = new ESLint({
+      cwd,
+      overrideConfig:vueConfig
+    })
+    const rules = await eslint.lintFiles(['./src/**/*.js','./src/**/*.vue'])
     const formatter = await eslint.loadFormatter('stylish')
     const resultText = formatter.format(rules)
-    console.log(resultText,11);
+    const ESLintResult = await this.parseESLintResult(resultText)
+    log.verbose('ESLintResult',ESLintResult)
+  }
+
+  async handleResult (resultText,type) {
+    const problems = resultText.match(/([0-9]+) problems/)[0].match(/[0-9]+/)[0]
+    const errors = resultText.match(/([0-9]+) errors/)[0].match(/[0-9]+/)[0]
+    const warnings = resultText.match(/([0-9]+) warnings/)[0].match(/[0-9]+/)[0]
+    switch (type) {
+      case 'problems':
+        return problems
+      case 'errors':
+        return errors
+      case 'warnings':
+        return warnings
+      default:
+        return null
+    }
+  }
+  async parseESLintResult (resultText) {
+    const problems =await this.handleResult(resultText,'problems')
+    const errors =await this.handleResult(resultText,'errors')
+    const warnings =await this.handleResult(resultText, 'warnings')
+    return {
+      problems: +problems || 0,
+      errors: +errors || 0,
+      warnings: +warnings || 0,
+    }
   }
 }
 
